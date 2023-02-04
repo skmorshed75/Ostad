69afbf30c57f4e0373a78dd3156d3c8bb7abfc08
Morshed <skmorshed75@yahoo.com>
2023-02-04 23:30:56 +0600

    PHP Class 14

---
+++ class16.php
@@ -0,0 +1,47 @@
+<?php
+//More on switch case
+
+$n = -21;
+$r = $n % 2;
+
+/*
+switch($r) {
+    case 0 :
+        switch($n) {
+            case $n > 0:
+                echo "$n is a positive even number";
+                break;
+            case $n<0:
+                echo "$n is a negative even number";
+                break;
+        }
+        break;
+    default:
+    switch($n){
+        case $n>0:
+            echo "$n is a positive odd number";
+            break;
+        case $n<0:
+            echo "$n is a negative odd number";
+            break;
+    }
+}
+*/
+
+$n = -21;
+$r = $n % 2;
+
+switch(true){
+    case (0 == $r && $n>0) :
+        echo "$n is a positive even number";
+        break;
+    case (1 == $r && $n>0) :
+        echo "$n is a positive odd number";
+        break;
+    case (0 == $r && $n<0) :
+        echo "$n is a negative even number";
+        break;
+    case (-1 == $r && $n<0) :
+        echo "$n is a negative odd number";
+        break;
+}
