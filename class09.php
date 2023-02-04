708644682f32042803aedbc74a8608dea5c904e3
Morshed <skmorshed75@yahoo.com>
2023-02-04 19:21:00 +0600

    PHP Class 09

--- class09.php
+++ class09.php
@@ -12,3 +12,12 @@ printf('The binary equivalent of %1$d is %1$b', 10);
 echo "\n";
 $n = 30.128; //convert to 2 digits after decimal
 printf("%.2f", $n);
+
+$m = 127; //0127
+$n = 23; //0023
+echo "\n";
+printf("%04d \n",$m);
+printf("%04d \n",$n);
+
+$m = 127.34; //0127.35
+printf("%07.2f \n", $m);
