fd353256cc68c6cf13805b85e311e3bbdb37e266
Morshed <skmorshed75@yahoo.com>
2023-02-06 01:10:01 +0600

    Assignment Module 02 & some changes on class16

--- class16.php
+++ class16.php
@@ -6,25 +6,25 @@ $r = $n % 2;
 
 /*
 switch($r) {
-    case 0 :
-        switch($n) {
-            case $n > 0:
-                echo "$n is a positive even number";
-                break;
-            case $n<0:
-                echo "$n is a negative even number";
-                break;
-        }
-        break;
-    default:
-    switch($n){
-        case $n>0:
-            echo "$n is a positive odd number";
-            break;
-        case $n<0:
-            echo "$n is a negative odd number";
-            break;
-    }
+	case 0 :
+		switch($n) {
+			case $n > 0:
+				echo "$n is a positive even number";
+				break;
+			case $n<0:
+				echo "$n is a negative even number";
+				break;
+		}
+		break;
+	default:
+	switch($n){
+		case $n>0:
+			echo "$n is a positive odd number";
+			break;
+		case $n<0:
+			echo "$n is a negative odd number";
+			break;
+	}
 }
 */
 
@@ -32,16 +32,16 @@ $n = -21;
 $r = $n % 2;
 
 switch(true){
-    case (0 == $r && $n>0) :
-        echo "$n is a positive even number";
-        break;
-    case (1 == $r && $n>0) :
-        echo "$n is a positive odd number";
-        break;
-    case (0 == $r && $n<0) :
-        echo "$n is a negative even number";
-        break;
-    case (-1 == $r && $n<0) :
-        echo "$n is a negative odd number";
-        break;
+	case (0 == $r && $n>0) :
+		echo "$n is a positive even number";
+		break;
+	case (1 == $r && $n>0) :
+		echo "$n is a positive odd number";
+		break;
+	case (0 == $r && $n<0) :
+		echo "$n is a negative even number";
+		break;
+	case (-1 == $r && $n<0) :
+		echo "$n is a negative odd number";
+		break;
 }
