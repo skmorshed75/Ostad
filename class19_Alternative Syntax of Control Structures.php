61c4ff3b23ce1f0c0cd576ff2beafd545522d13a
Morshed <skmorshed75@yahoo.com>
2023-02-06 20:28:33 +0600

    class19_Alternative Syntax of Control Structures

---
+++ class19_Alternative Syntax of Control Structures.php
@@ -0,0 +1,22 @@
+<?php
+//Alternative Syntax of Control Structures
+
+$n = 13;
+
+if($n % 2 == 0) {
+    echo "Even number";
+} else {
+    echo "Odd number";
+}
+
+//ALTERNATE SYNTAX
+echo PHP_EOL;
+if($n % 2 == 0) :
+    echo "Even Number";
+    echo PHP_EOL;
+    echo "Some Text";
+else :
+    echo "Odd Number";
+    echo PHP_EOL;
+    echo "Some more Text";
+endif;
