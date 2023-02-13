81ef97866b78e241a1bd980528b3d090539bb68c
Morshed <skmorshed75@yahoo.com>
2023-02-13 23:27:49 +0600

    Things to keep in mind with in while loops

---
+++ Things to keep in mind with in while loops.php
@@ -0,0 +1,25 @@
+<?php
+// Things to keep in mind with in while loops.php
+
+$i=0;
+while($i < 5){
+    echo $i;
+    echo PHP_EOL;
+    $i++;
+}
+
+echo "===================\n";
+
+$j=0;
+while($j++ <5){
+    echo $j;
+    echo PHP_EOL;
+}
+
+echo "===================\n";
+
+$j=0;
+while(++$j <5){
+    echo $j;
+    echo PHP_EOL;
+}
