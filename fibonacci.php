25bd5810e8e582d8de76a19ad086938b5f484d14
Morshed <skmorshed75@yahoo.com>
2023-02-16 21:41:28 +0600

    Added Alternative Fibonacci Rules

--- fibonacci.php
+++ fibonacci.php
@@ -4,9 +4,31 @@
 $veryOld = 0;
 $old = 1;
 $new = 1;
-for($x = 0; $x <=20; $x++){
+for($x = 0; $x <=10; $x++){
     echo $veryOld." ";
     $old = $new;
     $new = $old+$veryOld;
     $veryOld = $old;
 }
+
+echo PHP_EOL;
+echo "-------------------"."\n";
+//Alternative
+function fibonacci($old, $new, $end){
+    static $start;
+    $start == $start ?? 1;
+    if($start > $end){
+        return;
+    }
+    $start++;
+
+    echo $old." ";
+    $_temp = $old+$new;
+    $old = $new;
+    $new = $_temp;
+    fibonacci($old,$new,$end);
+}
+fibonacci(0,1,10);
+
+
+
