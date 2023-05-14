4cdb6caf09f8b54331e7ddf9e1b8be580f443aee
Morshed <skmorshed75@yahoo.com>
2023-05-15 00:17:17 +0600

    Module 13 Assignment

--- module-12/livetest/livetest.js
+++ module-12/livetest/livetest.js
@@ -1,35 +1,23 @@
-//MODULE 11, LIVE TEST
+//MODULE 12, LIVE TEST
 //Sheikh Mohammed Morshed
 /*
 Question
-
- Write a function called "gradeCalculator" that takes a numerical grade (out of 100) as input and returns a letter grade (A, B, C, D, or F) based on the following grading scale:
-  A: 90-100
-  B: 80-89
-  C: 70-79
-  D: 60-69
-  F: 0-59
-If the input is not a number between 0 and 100, return an error message.
-
-// Test Data:
-
-// The function should return "B" when given the input 85.
+Write a JavaScript function that selects an element with an id of "myList" and adds a new list item with the text "Apples" to the end of the list
+ 
 */
 
-function gradeCalculator(grade) {
-  if (isNaN(grade) || grade < 0 || grade > 100) {
-    return "Error: Invalid grade input";
-  } else if (grade >= 90) {
-    return "A";
-  } else if (grade >= 80) {
-    return "B";
-  } else if (grade >= 70) {
-    return "C";
-  } else if (grade >= 60) {
-    return "D";
-  } else {
-    return "F";
-  }
+function addListItem() {
+  // Get the unordered list element with an id of "myList"
+  const myList = document.getElementById("myList");
+
+  // Create a new list item element
+  const newListItem = document.createElement("li");
+
+  // Set the text of the new list item to "Apples"
+  newListItem.innerText = "Apples";
+
+  // Add the new list item to the end of the unordered list
+  myList.appendChild(newListItem);
 }
 
-console.log(gradeCalculator(100));
+console.log("Mango, ammmsdfsdfsdsdf");
